8d8f49a56fb22b7644645b77c56f2ca744d2b87d
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2013-09-26 14:33:13 +0300

    Add twitter bootstrap and jquery

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
 require_once __DIR__.'/Comment.php';
 
-$comment = new Comment('test13');
-//print $comment;
-//print $comment->save();
-
 include __DIR__.'/templates/main.php';
--- templates/main.php
+++ templates/main.php
@@ -4,21 +4,38 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>Гостевая книга</title>
+
+<!--        <script src="/lib/jquery/jquery-1.10.1.min.js"></script>-->
+        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+        <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <link rel="stylesheet" href="/lib/bootstrap/css/bootstrap.css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="/lib/bootstrap/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="/lib/bootstrap/css/bootstrap-theme.min.css">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="/lib/bootstrap/js/bootstrap.min.js"></script>
     </head>
 
     <body>
         <?php
-        if (!empty($_POST['comment'])) {
-            $comment = new Comment($_POST['comment']);
+        if (!empty($_POST['message'])) {
+            $comment = new Comment($_POST['message']);
             print '<span style="color:green">' . $comment->save() . '</span>';
-            unset($_POST['comment']);
+            unset($_POST['message']);
         }
         ?>
-        <form action="" method="POST">
-            <h3>Добавить новый комментарий</h3>
-            <textarea cols="50" name="comment"></textarea><br>
-            <input type="submit" value="Добавить комментарий" />
-            <input type="reset" value="Отмена" />
+
+        <form class="well span8" method="POST" action="">
+            <div class="row">
+                <div class="span5">
+                    <label>Message</label>
+                    <textarea name="message" id="message" class="input-xlarge span5" rows="10"></textarea>
+                </div>
+
+                <button type="reset" class="btn btn-primary pull-right">Отмена</button>
+                <button type="submit" class="btn btn-primary pull-right">Добавить комментарий</button>
+            </div>
         </form>
 
         <div>
